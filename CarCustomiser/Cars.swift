19ec5fd90222ddb4f787aa74fd5a194a0be9a6cd
Parker-F <parker.f@etoncollege.org.uk>
2024-01-22 22:50:57 +0000

    Penultimate Commit of the EW, I cant actually preview my code because there is an error that has been there since the start and still doesnt work. So theoretically, the rest of my code works.

--- CarCustomiser/Cars.swift
+++ CarCustomiser/Cars.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct StarterCars {
-    let cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5),
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5),
                 Car(make: "Delorean", model: "DMC-12", topSpeed: 88, acceleration: 9.5, handling: 8),
                 Car(make: "Cadillac", model: "Ecto-1", topSpeed: 150, acceleration: 4, handling: 4),
                 Car(make: "Pontiac", model: "1982 Trans Am", topSpeed: 124, acceleration: 7.2, handling: 7)]
--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,19 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
-    let starterCars = StarterCars()
-    @State private var selectedCar: Int = 0
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 20){
-            Text("\(StarterCars.cars
-                [selectedCar].displayStats())")
-            Button("Random Car", action: {
-                selectedCar = Int.random(in: 0
-                    ..<
-                    self.starterCars.cars.count)
-            })
+    @State private var starterCars = StarterCars()
+    @State private var selectedCar: Int = 0 {
+        didSet {
+            if selectedCar >= starterCars.cars.count {
+                selectedCar = 0
+            }
         }
+    }
+    @State private var exhaustPackage = false
+    @State private var tiresPackage = false
+    @State private var enginePackage = false
+    @State private var gearsPackage = false
+    @State private var remainingFunds = 1000
+    
+    var exhaustPackageEnabled: Bool {
+        return true
+        // custom code
+    }
+
+
+    var body: some View {
+        let exhaustPackageBinding = Binding<Bool> (
+            get : { self.exhaustPackage },
+            set : { newValue in
+                self.exhaustPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 5
+                    remainingFunds -= 500
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 5
+                    remainingFunds += 500
+
+                }
+            }
+        )
+        let tiresPackageBinding = Binding<Bool> (
+            get : { self.tiresPackage },
+            set : { newValue in
+                self.tiresPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].handling += 2
+                    remainingFunds -= 500
+
+                } else {
+                    starterCars.cars[selectedCar].handling -= 2
+                    remainingFunds += 500
+
+                }
+            }
+        )
+        
+        let enginePackageBinding = Binding<Bool> (
+            get : { self.enginePackage },
+            set : { newValue in
+                self.enginePackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 10
+                    starterCars.cars[selectedCar].acceleration -= 0.6
+                    remainingFunds -= 1000
+
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 10
+                    starterCars.cars[selectedCar].acceleration += 0.6
+                    remainingFunds += 1000
+
+                }
+            }
+        )
+        let gearsPackageBinding = Binding<Bool> (
+            get : { self.gearsPackage },
+            set : { newValue in
+                self.gearsPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].acceleration -= 1.2
+                    remainingFunds -= 750
+                } else {
+                    starterCars.cars[selectedCar].acceleration += 1.2
+                    remainingFunds += 750
+
+                }
+            }
+        )
+        VStack {
+            Form {
+                VStack(alignment: .leading, spacing: 20){
+                    Text("\(StarterCars.cars[selectedCar].displayStats())")
+                    Button("Random Car", action: {
+                        selectedCar += 1
+                        resetDisplay()
+                    })
+                }
+                Section{
+                    Toggle("Exhaust Package (Cost: 500)", isOn: exhaustPackageBinding)
+                        .disabled(!exhaustPackageEnabled)
+                    Toggle("Tires Package (Cost: 500)", isOn: tiresPackageBinding)
+                    Toggle("Engine Package (Cost: 1000)", isOn: enginePackageBinding)
+                    Toggle("Gears Package (Cost: 750)", isOn: gearsPackageBinding)
+                }
+            }
+            Text("Remaining Funds: \(remainingFunds)")
+                .foregroundColor(.red)
+                .baselineOffset(20)
+            
+        }
+    }
+    
+    func resetDisplay() {
+        
     }
 }
  
